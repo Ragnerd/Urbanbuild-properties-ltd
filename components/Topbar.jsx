5a7e23cda2b64e5f43593bdfbc4ec41446861727
Ragnerd <foysalakondo97@gmail.com>
2025-09-06 02:21:15 +0600

    topbar

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -2,7 +2,7 @@ import { RiPhoneFill, RiMainFill } from "react-icons/ri";
 
 const Topbar = () => {
   return (
-    <section id="home" className="py-4 xl:h-50">
+    <section id="home" className="py-4 xl:h-16">
       <div className="container mx-auto">Topbar</div>
     </section>
   );
